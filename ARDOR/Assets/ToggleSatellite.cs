18cb1cd3517458b79521246963f21058aa0bf2a8
izi2bhappy <israelkariti@gmail.com>
2022-05-29 15:17:28 +0300

    Revert "expand map to full screen"
    This reverts commit b6f3425be6581261c48c5103ab0d9a717d2afa4c.

--- ARDOR/Assets/ToggleSatellite.cs
+++ ARDOR/Assets/ToggleSatellite.cs
@@ -1,29 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using Mapbox.Unity.Map;
 public class ToggleSatellite : MonoBehaviour
 {
     public AbstractMap abstractMap;
-    public GameObject image;
-    public Sprite street;
-    public Sprite satellite;
-
     public void toggleSatellite(bool b)
     {
-
         IImageryLayer imageLayer = abstractMap.ImageLayer;
         if (imageLayer.LayerSource == ImagerySourceType.MapboxSatellite)
-        {
             imageLayer.SetLayerSource(ImagerySourceType.MapboxStreets);
-            image.GetComponent<Image>().sprite = satellite;
-        }
         else
-        {
             imageLayer.SetLayerSource(ImagerySourceType.MapboxSatellite);
-            image.GetComponent<Image>().sprite = street;
-
-        }
     }
 }
