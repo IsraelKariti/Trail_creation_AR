9af53a9cd326b1d4809f8731908e695e195c71f1
izi2bhappy <israelkariti@gmail.com>
2022-05-29 13:31:45 +0300

    add thumbnail for street to satellite

--- ARDOR/Assets/ToggleSatellite.cs
+++ ARDOR/Assets/ToggleSatellite.cs
@@ -2,15 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mapbox.Unity.Map;
+using UnityEngine.UI;
 public class ToggleSatellite : MonoBehaviour
 {
     public AbstractMap abstractMap;
+    public GameObject image;
+    public Sprite street;
+    public Sprite satellite;
+
     public void toggleSatellite(bool b)
     {
+
         IImageryLayer imageLayer = abstractMap.ImageLayer;
         if (imageLayer.LayerSource == ImagerySourceType.MapboxSatellite)
+        {
             imageLayer.SetLayerSource(ImagerySourceType.MapboxStreets);
+            image.GetComponent<Image>().sprite = satellite;
+        }
         else
+        {
             imageLayer.SetLayerSource(ImagerySourceType.MapboxSatellite);
+            image.GetComponent<Image>().sprite = street;
+
+        }
     }
 }
